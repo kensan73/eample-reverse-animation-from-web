6092ab0832f019675b332c74efc13fdfff8e31b3
Ken Ore <v-kore@expediagroup.com>
2021-12-05 07:39:32 -0600

    Manual animation toggle back forward working.

--- index.js
+++ index.js
@@ -1,21 +1,54 @@
 span = document.querySelector('span')
 button = document.querySelector('button')
-duration = 10; // animation-during
+timerElement = document.querySelector('#timerId')
+duration = 5; // animation-during
 
-let startTime;
+let startTime = Date.now();
+toSec = (msec) => msec / 1000
+elapsedTimeMsec = () => Date.now() - startTime
+elapsedTimeSec = () => toSec(elapsedTimeMsec())
+updateTimer = () =>     timerElement.innerHTML = `${elapsedTimeSec().toPrecision(2)}s`
+
+let intervalHandle;
+
+startTimer = () => {
+    intervalHandle = window.setInterval(() => {
+        updateTimer()
+    }, 500)
+}
+
+endTimer = () => {
+    window.clearInterval(intervalHandle)
+    intervalHandle = null
+}
 
 span.addEventListener('animationstart', () => {
     startTime = Date.now();
-    button.style.visibility = 'visible';
+    startTimer()
 });
 
 span.addEventListener('animationiteration', () => span.style.animationPlayState = 'paused');
 
+toggleAnimation = () => {
+    span.classList.remove('my_anim');
+    void span.offsetWidth; // safely apply changes
+    span.classList.add('my_anim');
+    if(span.style.animationDirection !== 'reverse')
+        span.style.animationDirection = 'reverse';
+    else
+        span.style.animationDirection = 'normal';
+    span.style.animationPlayState = 'running';
+}
 span.addEventListener('animationend', () => {
-    // button.style.visibility = 'hidden';
+    // console.log('animationend')
+    endTimer()
+    updateTimer()
+
+    toggleAnimation();
 });
 
 button.addEventListener('click', () => {
+    // wont work anymore since i changed anim to forward
     span.classList.remove('my_anim');
     void span.offsetWidth; // safely apply changes
     span.classList.add('my_anim');
