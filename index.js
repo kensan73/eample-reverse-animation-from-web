d69e09d55a0f6fbbbd010539b45274c35dfee64a
Ken Ore <v-kore@expediagroup.com>
2021-12-04 19:21:26 -0600

    Tighted up code for simplicity.

--- index.js
+++ index.js
@@ -17,30 +17,26 @@ span.addEventListener('animationstart', () => {
 
 span.addEventListener('animationiteration', () => console.log('animationiteration'));
 
-function toggleReverse() {
+function toggleReverse(delay) {
     span.classList.remove('my_anim');
-    void span.offsetWidth; // safely apply changes
+    void span.offsetWidth;
     span.classList.add('my_anim');
-    span.style.animationDelay = `0s`;
+    if(delay == null) {
+        span.style.animationDelay = `0s`;
+    } else {
+        span.style.animationDelay = `${delay}s`;
+    }
     console.log('animationend before running')
     span.style.animationPlayState = 'running';
 }
 
 span.addEventListener('animationend', () => {
     console.log('animationend')
-    // button.style.visibility = 'hidden';
-
     toggleReverse();
 });
 
 button.addEventListener('click', () => {
-    span.classList.remove('my_anim');
-    void span.offsetWidth; // safely apply changes
-    span.classList.add('my_anim');
-    console.log('click start: ' + startTime)
     const elapsed = Date.now() - startTime;
     const delay = (elapsed < toMsecs(duration)) ? (toSecs(elapsed) - duration * 2) : -duration;
-    span.style.animationDelay = `${delay}s`;
-    console.log('click before running')
-    span.style.animationPlayState = 'running';
+    toggleReverse(delay)
 });
