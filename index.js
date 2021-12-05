aabe2b270da0c04bf20c20802388294b7857e436
Ken Ore <v-kore@expediagroup.com>
2021-12-04 20:20:26 -0600

    Can reverse only on phase 1 of forward, not phase 2 > 10s of forward.

--- index.js
+++ index.js
@@ -1,21 +1,41 @@
 span = document.querySelector('span')
 button = document.querySelector('button')
+timeDiv = document.querySelector('#timeElapsedId')
 duration = 10; // animation-during
 
-let startTime;
+let startTime = Date.now();
 
 const oneThousand = 1000
+
 toMsecs = (secs) => oneThousand * secs
 toSecs = (msecs) => msecs / oneThousand
+timeElapsed = (startTime) => toSecs(Date.now() - startTime).toPrecision(2)
+isForward = () => {
+    return timeElapsed(startTime) < 10;
+}
+
+setInterval(() => {
+    timeDiv.innerHTML = `${timeElapsed(startTime)}s`
+}, 500)
 
 span.addEventListener('animationstart', () => {
-    console.log('animationstart')
     startTime = Date.now();
-    console.log('animationstart start: ' + startTime)
-    button.style.visibility = 'visible';
+    console.log('isForward: ' + isForward())
 });
 
-span.addEventListener('animationiteration', () => console.log('animationiteration'));
+span.addEventListener('animationiteration', () => {
+    console.log('isForward: ' + isForward())
+});
+
+function replaceAnim() {
+    span.classList.remove('my_anim');
+    void span.offsetWidth;
+    span.classList.add('my_anim');
+    const calculatedDelay = timeElapsed(startTime) - 10
+    span.style.animationDelay = `-${calculatedDelay}s`;
+    span.style.animationDirection = 'alternate-reverse';
+    span.style.animationPlayState = 'running';
+}
 
 function toggleReverse(delay) {
     span.classList.remove('my_anim');
@@ -26,17 +46,22 @@ function toggleReverse(delay) {
     } else {
         span.style.animationDelay = `${delay}s`;
     }
-    console.log('animationend before running')
     span.style.animationPlayState = 'running';
 }
 
 span.addEventListener('animationend', () => {
-    console.log('animationend')
     toggleReverse();
 });
 
 button.addEventListener('click', () => {
-    const elapsed = Date.now() - startTime;
-    const delay = (elapsed < toMsecs(duration)) ? (toSecs(elapsed) - duration * 2) : -duration;
-    toggleReverse(delay)
+    if(!isForward()) {
+        // replaceAnim()
+        // const elapsed = Date.now() - startTime;
+        // const delay = (elapsed < toMsecs(duration)) ? (toSecs(elapsed) - duration * 2) : -duration;
+        // toggleReverse(delay)
+    } else {
+        const elapsed = Date.now() - startTime;
+        const delay = (elapsed < toMsecs(duration)) ? (toSecs(elapsed) - duration * 2) : -duration;
+        toggleReverse(delay)
+    }
 });
